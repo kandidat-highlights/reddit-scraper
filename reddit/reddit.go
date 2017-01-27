81b1af5ac71ef1d5246dc26419f33c3ef9b78fbd
Alexander Håkansson <al3xand3r.h@gmail.com>
2017-01-27 15:56:07 +0100

    Makes sure reddit request rates are taken into account

--- reddit/reddit.go
+++ reddit/reddit.go
@@ -2,6 +2,7 @@ package reddit
 
 import (
 	"strings"
+	"time"
 )
 
 // PostInfo represents info about a post that a user has voted on
@@ -12,8 +13,41 @@ type PostInfo struct {
 	Content  string
 }
 
+var (
+	rateUsed                    = 0
+	rateRemaining               = 60
+	rateReset     time.Duration = 60
+)
+
 // GetPostInfo processes a line in the csv and returns a PostInfo struct
 func GetPostInfo(input string) PostInfo {
+	response := new(PostInfo)
+
+	// Process input
 	data := strings.Split(input, ",")
-	return PostInfo{data[0], data[2], data[1][3:], "No content"}
+	username := data[0]
+	vote := data[2]
+	fullname := data[1]
+	response.Username = username
+	response.Vote = vote
+
+	var title, content string
+
+	// Get data from Reddit
+	if rateRemaining > 0 {
+		// Make request
+		title, content = getRedditInfo(fullname)
+	} else {
+		// Wait until new period
+		time.Sleep(rateReset * time.Second)
+		// Make request
+		title, content = getRedditInfo(fullname)
+	}
+	response.Title = title
+	response.Content = content
+	return *response
+}
+
+func getRedditInfo(fullname string) (title, content string) {
+	return
 }
